b0edeb1b8db596385a86e794e9645a10b81bb059
Iohann-Sanmartin <iohann.sanmartin@uclouvain.be>
2026-01-25 19:16:48 +0100

    n+1 corregido

--- fibonacci.py
+++ fibonacci.py
@@ -19,21 +19,24 @@
 
 
 def fiborecu(s):
-   
+    s=s-1
+    if s == 0:
+        print("base uno. STOP")
+        return [0]
     if s == 1:
-        #print("base uno. STOP")
-        return [0,1]
+        print("base uno. STOP")
+        return [0,1] # aca uno SIEMPRE debe poner lo que necesita como punto de partida del calculo
     
-    #print ("entra a recursion:", s)
+    print ("entra a recursion:", s)
     lista = fiborecu(s-1)
-    #print ("sale de recursion:", s)
-    #print("lista:", lista)
-    #print(f"sumando {lista[-1]} y {lista[-2]} para hacer append")
+    print ("sale de recursion:", s)
+    print("lista:", lista)
+    print(f"sumando {lista[-1]} y {lista[-2]} para hacer append")
     lista.append(lista[-1] + lista[-2]) # est hace que el ultimo valor de la lista se sume al penultimo valor y lo adiciona al final de la lista.
-    #print("lista despues del append:", lista)
+    print("lista despues del append:", lista)
     return lista #este return  entrega la MISMA lista. El valor viene de f==0 o f==1
 
-print(fiborecu(5))
+print(fiborecu(2))
 
 '''El fiborecu funciona porque en la recursion, el ultimo que entra es el primero que se resuelve y sale. 
 De esta forma recursion siempre baja por la izquierda (es decir, todos los fiborecu (n-2)),
